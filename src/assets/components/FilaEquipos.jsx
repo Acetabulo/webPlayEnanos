2e8ae77d8454eaf4aa4f753d6619d483c17eda51
Santiago Chicaiza <schicaij@gmail.com>
2024-05-27 08:54:59 -0500

    Solo se ordena formato, terminado hasta eliminacion de filas

--- src/assets/components/FilaEquipos.jsx
+++ src/assets/components/FilaEquipos.jsx
@@ -1,33 +1,34 @@
 import React, { useContext } from "react";
-import {Tr, Td} from '../css/Formats'
+import { Tr, Td } from "../css/Formats";
 import { equiposContext } from "./Contextos";
-export const FilaEquipos = ({team, pinEquipo}) => {
-const {eliminaEquipo} = useContext(equiposContext)
-const limpiarFila=()=>{
-  //e.preventDefault();
-  if(window.confirm('Seguro de borrar registro')){
-    eliminaEquipo(team);
-  }
-}
-  return (
-      <Tr key={team.equipo}>
-        <Td>{team.equipo}</Td>
-        <Td>{team.jugador}</Td>
-        <Td>
-          <input
-            type='checkbox'
-            checked={team.pin}
-            onChange={() => pinEquipo(team)}
-          />
-        </Td>
-        
-        {!team.pin&&(
-          <Td><button >E</button></Td>
-        )}
-  
-        <Td><button onClick={limpiarFila}> C </button></Td>
-              
-      </Tr>
-    );
+export const FilaEquipos = ({ team, pinEquipo }) => {
+  const { eliminaEquipo } = useContext(equiposContext);
+  const limpiarFila = () => {
+    if (window.confirm("Seguro de borrar registro")) {
+      eliminaEquipo(team);
+    }
   };
-  
+  return (
+    <Tr key={team.equipo}>
+      <Td>{team.equipo}</Td>
+      <Td>{team.jugador}</Td>
+      <Td>
+        <input
+          type='checkbox'
+          checked={team.pin}
+          onChange={() => pinEquipo(team)}
+        />
+      </Td>
+      {!team.pin && (
+        <Td>
+          <button>E</button>
+        </Td>
+      )}
+       {!team.pin && (
+      <Td>
+        <button onClick={limpiarFila}> C </button>
+      </Td>
+       )}
+    </Tr>
+  );
+};
